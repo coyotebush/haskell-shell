dabb6ce36765feb144512de1a2b75be865a2ba26
Corey Ford <coyotebush22@gmail.com>
2012-06-04 16:24:33 -0700

    basic pwd and cd builtins

--- sh.hs
+++ sh.hs
@@ -1,4 +1,5 @@
 #!/usr/bin/env runhaskell
+import System.Directory
 import System.IO
 import System.IO.Error
 
@@ -11,8 +12,19 @@ shellLoop = do
             hFlush stdout
             input <- try (getLine)
             case input of
-              Left e -> if isEOFError e
-                        then return ()
-                        else ioError e
-              Right inStr -> shellLoop
+              Left e ->
+                if isEOFError e
+                then return ()
+                else ioError e
+              Right inStr ->
+                do
+                case inStr of
+                  "cd"  -> do
+                           dir <- getHomeDirectory
+                           setCurrentDirectory dir
+                  "pwd" -> do
+                           dir <- getCurrentDirectory
+                           putStrLn dir
+                  ""    -> return ()
+                shellLoop
 
