65b98b0f762d6fb1b720bbfbf8ce1a98d7792fe1
Corey Ford <coyotebush22@gmail.com>
2012-06-04 15:55:15 -0700

    rename test files, catch with glob

--- test.sh
+++ test.sh
@@ -1,18 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 EXE1='bash --norc --noediting -i'
 EXE2='./sh.hs'
-TESTS='
-001prompt
-002multiprompt
-'
 DIFF='diff -U3'
 
 set -e
-for t in $TESTS
+for t in tests/[0-9]*
 do
     echo Running test case $t
-    PS1='$ ' $EXE1 < tests/$t.in > tests/output.1 2>&1
-    $EXE2 < tests/$t.in > tests/output.2
+    PS1='$ ' $EXE1 < $t > tests/output.1 2>&1
+    $EXE2 < $t > tests/output.2
     $DIFF tests/output.1 tests/output.2
 done
