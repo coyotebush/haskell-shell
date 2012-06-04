cc12c08fa53bc7ca028e8b7d9a65dccdea15359a
Corey Ford <coyotebush22@gmail.com>
2012-06-04 15:13:52 -0700

    add multiprompt test

--- test.sh
+++ test.sh
@@ -2,12 +2,16 @@
 
 EXE1='bash --norc --noediting -i'
 EXE2='./sh.hs'
-TESTS='1prompt'
+TESTS='
+001prompt
+002multiprompt
+'
 DIFF='diff -U3'
 
 set -e
 for t in $TESTS
 do
+    echo Running test case $t
     PS1='$ ' $EXE1 < tests/$t.in > tests/output.1 2>&1
     $EXE2 < tests/$t.in > tests/output.2
     $DIFF tests/output.1 tests/output.2
