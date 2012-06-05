30075297a30d6b1132aa7fc06fb91b85f06ec2c1
Corey Ford <coyotebush22@gmail.com>
2012-06-04 21:28:40 -0700

    basic external processes with System.Process

--- HaskellShell/Run.hs
+++ HaskellShell/Run.hs
@@ -1,4 +1,6 @@
 module HaskellShell.Run (runList) where
+import qualified Control.Monad as M
+import qualified System.Process as P
 import HaskellShell.Builtins
 import qualified HaskellShell.Grammar as G
 
@@ -12,5 +14,8 @@ runCommand :: G.Command -> IO ()
 runCommand cmd | length cmd > 0 = do
                                   case lookup (head cmd) builtins of
                                     Just builtin -> builtin (tail cmd)
-                                    Nothing -> return ()
+                                    Nothing -> do
+                                               (_, _, _, p) <- P.createProcess
+                                                 (P.proc (head cmd) (tail cmd))
+                                               M.void $ P.waitForProcess p
 
