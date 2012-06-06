7a95b440a1f02e3f710ab745e7fecf8ca0dc0b48
Corey Ford <coyotebush22@gmail.com>
2012-06-05 17:00:17 -0700

    more robust runCommand

--- HaskellShell/Run.hs
+++ HaskellShell/Run.hs
@@ -11,11 +11,12 @@ runPipeline :: G.Pipeline -> IO ()
 runPipeline = mapM_ runCommand
 
 runCommand :: G.Command -> IO ()
-runCommand cmd | length cmd > 0 = do
-                                  case lookup (head cmd) builtins of
-                                    Just builtin -> runBuiltin builtin cmd
-                                    Nothing -> do
-                                               (_, _, _, p) <- P.createProcess
-                                                 (P.proc (head cmd) (tail cmd))
-                                               M.void $ P.waitForProcess p
+runCommand [] = return ()
+runCommand cmd = do
+                 case lookup (head cmd) builtins of
+                   Just builtin -> runBuiltin builtin cmd
+                   Nothing -> do
+                     (_, _, _, p) <- P.createProcess
+                       (P.proc (head cmd) (tail cmd))
+                     M.void $ P.waitForProcess p
 
