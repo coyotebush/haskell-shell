bf42dff747421ca789cfdc73aef245348bee1bf2
Corey Ford <coyotebush22@gmail.com>
2012-06-05 22:08:07 -0700

    initial work on flexible fds

--- HaskellShell/Grammar.hs
+++ HaskellShell/Grammar.hs
@@ -3,5 +3,6 @@ module HaskellShell.Grammar (Argument, Command, Pipe(..), Pipeline, List) where
 type Argument = String
 type Command = [Argument]
 data Pipe = Pipe | NoPipe deriving (Eq, Show)
-type Pipeline = [(Pipe, Command)]
+type PipelineElement = (Pipe, Command)
+type Pipeline = [PipelineElement]
 type List = [Pipeline]
--- HaskellShell/Run.hs
+++ HaskellShell/Run.hs
@@ -1,5 +1,6 @@
 module HaskellShell.Run (runList) where
 import qualified Control.Monad as M
+import System.IO (stdin, stdout, stderr)
 import qualified System.Process as P
 import HaskellShell.Builtins
 import qualified HaskellShell.Grammar as G
@@ -8,15 +9,19 @@ runList :: G.List -> IO ()
 runList = mapM_ runPipeline
 
 runPipeline :: G.Pipeline -> IO ()
-runPipeline = mapM_ (runCommand . snd)
+runPipeline = mapM_ (runCommand (P.UseHandle stdin) (P.UseHandle stdout) (P.UseHandle stderr) . snd)
 
-runCommand :: G.Command -> IO ()
-runCommand [] = return ()
-runCommand cmd = do
-                 case lookup (head cmd) builtins of
-                   Just builtin -> runBuiltin builtin cmd
-                   Nothing -> do
-                     (_, _, _, p) <- P.createProcess
-                       (P.proc (head cmd) (tail cmd))
-                     M.void $ P.waitForProcess p
+--runPipelineElement :: G.PipelineElement -> 
+
+runCommand :: P.StdStream -> P.StdStream -> P.StdStream -> G.Command -> IO ()
+runCommand _ _ _ []  = return ()
+runCommand i o e cmd = case lookup (head cmd) builtins of
+                       Just builtin -> runBuiltin o builtin cmd
+                       Nothing -> do
+                         (_, _, _, p) <- P.createProcess $
+                           (P.proc (head cmd) (tail cmd)) { P.std_in  = i
+                                                          , P.std_out = o
+                                                          , P.std_err = e
+                                                          }
+                         M.void $ P.waitForProcess p
 
