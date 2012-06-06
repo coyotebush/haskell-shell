6fa7dbd9503ba7d966b9f2c3a913ceec7de37e24
Corey Ford <coyotebush22@gmail.com>
2012-06-06 15:58:19 -0700

    lex based on possibly-escaped chars

--- HaskellShell/Parse/Lex.hs
+++ HaskellShell/Parse/Lex.hs
@@ -28,19 +28,37 @@ escapes []            = []
 escapes ('\\':c:rest) = Escaped c : escapes rest
 escapes (c:rest)      = Unescaped c : escapes rest
 
+isEscaped :: ShellChar -> Bool
+isEscaped (Escaped _) = True
+isEscaped (Unescaped _) = False
+
 unescape :: ShellChar -> Char
 unescape (Escaped c)   = c
 unescape (Unescaped c) = c
 
+literalUnescape :: ShellChar -> String
+literalUnescape (Escaped c)   = ['\\', c]
+literalUnescape (Unescaped c) = [c]
+
+isChar :: ShellChar -> Char -> Bool
+isChar (Unescaped c) c2 = c == c2
+isChar (Escaped _)   _  = False
+
+isAnyChar :: [Char] -> ShellChar -> Bool
+isAnyChar cs sc = any (isChar sc) cs
+
 lexInput :: String -> [ShellToken]
-lexInput "" = []
-lexInput (c:rem) | isSpace c            = Blank : (lexInput rem)
-                 | isQuote c            = let (cs, _:rest) = break (== c) rem in
-                                          (Quote c cs) : (lexInput rest)
-                 | [c] `elem` operators = let (o, rest) = takeOperator [c] rem in
-                                          (Operator o) : (lexInput rest)
-                 | otherwise            = let (cs, rest) = break (\x -> isSpace x || isQuote x || [x] `elem` operators) rem in
-                                     (Word (c:cs)) : (lexInput rest)
+lexInput = lexChars . escapes
+
+lexChars :: [ShellChar] -> [ShellToken]
+lexChars [] = []
+lexChars (c:rem) | isAnyChar spaces c  = Blank : (lexChars rem)
+                 | isAnyChar quotes c  = let (cs, _:rest) = break (== c) rem in
+                                         (Quote (unescape c) (concatMap literalUnescape cs)) : (lexChars rest)
+                 | isAnyChar opChars c = let (o, rest) = takeOperator' (c:rem) in
+                                         (Operator o) : (lexChars rest)
+                 | otherwise           = let (cs, rest) = break (isAnyChar (spaces ++ quotes ++ opChars)) rem in
+                                         (Word $ map unescape (c:cs)) : (lexChars rest)
 
 takeOperator :: String -> String -> (String, String)
 takeOperator o []     = (o, "")
@@ -49,14 +67,20 @@ takeOperator o (x:xs) = let n = o ++ [x] in
                         then takeOperator n xs
                         else (o, x:xs)
 
-isSpace = (==) ' '
-isQuote = flip elem "'\"`"
+takeOperator' :: [ShellChar] -> (String, [ShellChar])
+takeOperator' (x:xs) = let (u, r1) = break isEscaped xs
+                           (o, r2) = takeOperator [(unescape x)] (map unescape u)
+                       in  (o, r1 ++ (map Unescaped r2))
+
+spaces = " "
+quotes = "'\"`"
 
 -- based on bash(1) and dash(1) man pages
 listOperators = [ ";", "&", "&&", "||" ]
 pipelineOperators = [ "|", "|&" ]
 redirectionOperators = [ "<", ">", ">|", "<<", ">>", "<&", ">&", "<<-", "<>" ]
 operators = listOperators ++ pipelineOperators ++ redirectionOperators -- "(", ")", ";;"
+opChars = map head operators
 
 isOperator :: [String] -> ShellToken -> Bool
 isOperator ops (Operator s) = s `elem` ops
