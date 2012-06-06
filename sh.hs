8ac170bf5e13b9419bfe92707c466a35e135579f
Corey Ford <coyotebush22@gmail.com>
2012-06-05 18:43:25 -0700

    move 'exit' print into shellLoop

--- sh.hs
+++ sh.hs
@@ -6,15 +6,13 @@ import HaskellShell.Input
 import HaskellShell.Parse
 import HaskellShell.Run
 
-main = do
-       shellLoop
-       putStrLn "exit"
+main = shellLoop
 
 shellLoop = do
             input <- IOE.try promptInput
             case input of
-              Left e | IOE.isEOFError e -> return ()
-                     | otherwise -> ioError e
+              Left e | IOE.isEOFError e -> putStrLn "exit"
+                     | otherwise        -> ioError e
               Right inStr -> do
                              runList $ parseInput inStr
                              shellLoop
