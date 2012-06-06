80a6effab4787dddc9f72c290bbcc241ab8387b0
Corey Ford <coyotebush22@gmail.com>
2012-06-05 18:54:38 -0700

    move test script

---
+++ tests/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+EXE1='bash --norc --noediting -i'
+EXE2='./sh'
+DIFF='diff -U3'
+MAKE='ghc --make sh.hs'
+
+set -e
+
+if [ ! -z "$MAKE" ]; then $MAKE; fi
+
+# http://stackoverflow.com/a/246128/230170 
+TESTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+for t in tests/[0-9]*
+do
+    echo Running test case $t
+    PS1='$ ' $EXE1 < $t > $TESTDIR/output.1 2>&1
+    $EXE2 < $t | sed -e 's/sh.hs/bash/' > $TESTDIR/output.2
+    $DIFF $TESTDIR/output.1 $TESTDIR/output.2
+done
