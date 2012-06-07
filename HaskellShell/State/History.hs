bb080d17368b9a26392269f043e71e7deb5e8aa8
Corey Ford <coyotebush22@gmail.com>
2012-06-06 21:21:58 -0700

    don't make History a record, abstract HistoryEntry

--- HaskellShell/State/History.hs
+++ HaskellShell/State/History.hs
@@ -2,18 +2,17 @@ module HaskellShell.State.History where
 import Data.Foldable (toList)
 import qualified Data.Sequence as Seq
 
-data History = History { depth :: Int
-                       , entries :: (Seq.Seq String)
-                       }
+type HistoryEntry = String
+data History = History Int (Seq.Seq String)
                deriving Show
 
 emptyHistory :: Int -> History
-emptyHistory d = History { depth = d, entries = Seq.empty }
+emptyHistory d = History d Seq.empty
 
 addToHistory :: History -> String -> History
-addToHistory h s = h { entries = Seq.take (depth h) $ s Seq.<| entries h }
+addToHistory (History d xs) x = History d $ Seq.take d $ x Seq.<| xs
 
-getHistory :: History -> [String]
-getHistory = toList . Seq.reverse . entries
+getHistory :: History -> [HistoryEntry]
+getHistory (History _ xs) = toList $ Seq.reverse xs
 
 
