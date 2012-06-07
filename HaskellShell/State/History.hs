57924d0ef65f50675ae6e4bdbf34cc829a6a13aa
Corey Ford <coyotebush22@gmail.com>
2012-06-07 16:54:49 -0700

    just use a list for history

--- HaskellShell/State/History.hs
+++ HaskellShell/State/History.hs
@@ -1,20 +1,19 @@
 module HaskellShell.State.History where
 import Data.Foldable (toList)
-import qualified Data.Sequence as Seq
 
 type HistoryEntry = (Int, String)
-data History = History Int (Seq.Seq HistoryEntry)
+data History = History Int [HistoryEntry]
                deriving Show
 
 emptyHistory :: History
-emptyHistory = History 0 Seq.empty
+emptyHistory = History 0 []
 
 addToHistory :: History -> Maybe Int -> String -> History
-addToHistory (History n xs) d x = History (n + 1) $ maybeTake d $ (n + 1, x) Seq.<| xs
+addToHistory (History n xs) d x = History (n + 1) $ maybeTake d $ (n + 1, x):xs
                                   where maybeTake Nothing    = id
-                                        maybeTake (Just num) = Seq.take num
+                                        maybeTake (Just num) = take num
 
 getHistory :: History -> [HistoryEntry]
-getHistory (History _ xs) = toList $ Seq.reverse xs
+getHistory (History _ xs) = reverse xs
 
 
