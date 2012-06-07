2e2694e541061129e8a425e31877b6d3849e77eb
Corey Ford <coyotebush22@gmail.com>
2012-06-06 20:46:39 -0700

    allow shells to exit non-zero in testing

--- tests/run.sh
+++ tests/run.sh
@@ -5,9 +5,7 @@ EXE2='./sh'
 DIFF='diff -U3'
 MAKE='ghc --make sh.hs'
 
-set -e
-
-if [ ! -z "$MAKE" ]; then $MAKE; fi
+if [ ! -z "$MAKE" ]; then $MAKE || exit; fi
 
 # http://stackoverflow.com/a/246128/230170 
 TESTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -17,5 +15,5 @@ do
     echo Running test case $t
     PS1='$ ' $EXE1 < $t > $TESTDIR/output.1 2>&1
     $EXE2 < $t | sed -e 's/sh.hs/bash/' > $TESTDIR/output.2
-    $DIFF $TESTDIR/output.1 $TESTDIR/output.2
+    $DIFF $TESTDIR/output.1 $TESTDIR/output.2 || exit
 done
