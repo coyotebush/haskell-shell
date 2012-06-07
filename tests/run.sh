8f9cf0d3cb3d1b13c3a96cde3f4763ed67dc469b
Corey Ford <coyotebush22@gmail.com>
2012-06-06 23:16:16 -0700

    add setenv test

--- tests/run.sh
+++ tests/run.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-EXE1='bash --norc --noediting -i'
+# http://stackoverflow.com/a/246128/230170 
+TESTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+EXE1="bash --rcfile $TESTDIR/bashrc --noediting -i"
 EXE2='./sh'
 DIFF='diff -U3'
 MAKE='ghc --make sh.hs'
 
 if [ ! -z "$MAKE" ]; then $MAKE || exit; fi
 
-# http://stackoverflow.com/a/246128/230170 
-TESTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 for t in tests/[0-9]*
 do
     echo Running test case $t
-    PS1='$ ' HISTFILE= $EXE1 < $t > $TESTDIR/output.1 2>&1
+    HISTFILE= $EXE1 < $t > $TESTDIR/output.1 2>&1
     $EXE2 < $t | sed -e 's/sh.hs/bash/' > $TESTDIR/output.2
     $DIFF $TESTDIR/output.1 $TESTDIR/output.2 || exit
 done
