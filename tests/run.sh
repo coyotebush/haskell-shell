f00b79ed78bc2f9fdc8610c1a2c51b858832781a
Corey Ford <coyotebush22@gmail.com>
2012-06-06 21:16:01 -0700

    disable bash history file for testing

--- tests/run.sh
+++ tests/run.sh
@@ -13,7 +13,7 @@ TESTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 for t in tests/[0-9]*
 do
     echo Running test case $t
-    PS1='$ ' $EXE1 < $t > $TESTDIR/output.1 2>&1
+    PS1='$ ' HISTFILE= $EXE1 < $t > $TESTDIR/output.1 2>&1
     $EXE2 < $t | sed -e 's/sh.hs/bash/' > $TESTDIR/output.2
     $DIFF $TESTDIR/output.1 $TESTDIR/output.2 || exit
 done
