74b4fd671a2e9931965d7b0a4bcb344a8283e50f
Corey Ford <coyotebush22@gmail.com>
2012-06-07 23:13:56 -0700

    run pipes with new redirection grammar

--- HaskellShell/Run.hs
+++ HaskellShell/Run.hs
@@ -15,21 +15,33 @@ runList :: ShellState -> G.List -> IO ()
 runList st = mapM_ (runPipeline st)
 
 runPipeline :: ShellState -> G.Pipeline -> IO ()
-runPipeline st = runPipelineElements st stdin
+runPipeline st = runPipelineElements st P.Inherit
 --runPipeline = mapM_ (runCommand (P.P.UseHandle stdin) (P.P.UseHandle stdout) (P.P.UseHandle stderr) . snd)
 
-runPipelineElements :: ShellState -> Handle -> [G.PipelineElement] -> IO ()
-runPipelineElements st input ((cmd, G.NoPipe):[]) = M.void $ runCommand st (P.UseHandle input) (P.UseHandle stdout) (P.UseHandle stderr) cmd
-runPipelineElements st input ((cmd, G.Pipe):rem)  = do
-                                                 nextPipe <- runCommand st (P.UseHandle input) P.CreatePipe (P.UseHandle stderr) cmd
-                                                 case nextPipe of
-                                                   Just ph -> runPipelineElements st ph rem
-                                                   Nothing -> runPipelineElements st stdin rem
+runPipelineElements :: ShellState -> P.StdStream -> [G.PipelineElement] -> IO ()
+runPipelineElements _  _     [] = return ()
+--runPipelineElements input ((cmd, rs):[]) = M.void $ runCommand (P.UseHandle input) (P.UseHandle stdout) (P.UseHandle stderr) cmd
+runPipelineElements st input ((cmd, rs):rem)  = do
+                                             nextPipe <- runCommand st (getStream G.Input  rs input)
+                                                                    (getStream G.Output rs P.Inherit)
+                                                                    (getStream G.Error  rs P.Inherit)
+                                                                    cmd
+                                             case nextPipe of
+                                               Just ph -> runPipelineElements st (P.UseHandle ph) rem
+                                               Nothing -> runPipelineElements st P.Inherit rem
+                                        where getStream :: G.Stream -> [G.Redirection] -> P.StdStream -> P.StdStream
+                                              getStream s rs def = case lookup s rs of
+                                                                     Just G.Pipe -> P.CreatePipe
+                                                                     --Just G.File p
+                                                                     Nothing -> def
 
 runCommand :: ShellState -> P.StdStream -> P.StdStream -> P.StdStream -> G.Command -> IO (Maybe Handle)
 runCommand _ _ _ _ []  = return Nothing
 runCommand st i o e cmd = case lookup (head cmd) builtins of
                        Just builtin -> case o of
+                                         P.Inherit       -> do
+                                                            runBuiltin st stdout builtin cmd
+                                                            return Nothing
                                          (P.UseHandle h) -> do
                                                             runBuiltin st h builtin cmd
                                                             return Nothing
